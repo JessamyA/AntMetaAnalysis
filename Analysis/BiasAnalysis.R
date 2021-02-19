a5eac2b00df0ace9bafd70dd9c3762f0b5eee6e5
JessamyA <jsa529@york.ac.uk>
2021-02-19 10:54:10 +0000

    Graph and .txt datafile added for Species # in each Subfamily (In real world, data taken from AntWiki)

--- Analysis/BiasAnalysis.R
+++ Analysis/BiasAnalysis.R
@@ -7,6 +7,7 @@ library(ggplot2)
 #Import data 
 Raw <- read.table("Data/Raw Data.txt", header = TRUE, sep = "\t")
 SpeciesData <- read.table("Data/Species Level.txt", header = TRUE, sep = "\t")
+TotalSpeciesData <- read.table("Data/Subfamily Species.txt", header = TRUE, sep = "\t")
 
 #Blank theme for ggplot
 blank_theme <- theme_minimal() +
@@ -21,12 +22,42 @@ blank_theme <- theme_minimal() +
     plot.title = element_text(size = 14, face = "bold")
   )
 
-#Piechart for Subfamily occurrence
+#Piechart for Subfamily occurrence in Meta-Analysis
 ggplot(data = SpeciesData, aes(x = factor(1), fill = Subfamily)) +
-  geom_bar() +
+  geom_bar(color = "black") +
   coord_polar("y", start = 0) +
+  scale_fill_manual(values = c("#ff6b6b",
+                               "#ffb76b",
+                               "#ffff6b",
+                               "#7dff6b",
+                               "#6befff",
+                               "#6b6eff",
+                               "#cc6bff")) +
   blank_theme
   
+#Piechart for Subfamily occurrence in Total
+ggplot(data = TotalSpeciesData, aes(x = "", y = ExtantSpecies, fill = Subfamily)) +
+  geom_bar(color = "black", stat = "identity") +
+  coord_polar("y", start = 0) +
+  scale_fill_manual(values = c("Grey",
+                               "#ff6b6b",
+                               "Grey",
+                               "Grey",
+                               "#ffb76b",
+                               "#ffff6b",
+                               "#7dff6b",
+                               "#6befff",
+                               "Grey",
+                               "Grey",
+                               "Grey",
+                               "Grey",
+                               "#6b6eff",
+                               "Grey",
+                               "#cc6bff",
+                               "Grey",
+                               "Grey")) +
+  blank_theme
+
 #Piechart for Lab vs. Field studies
 ggplot(data = Raw, aes(x = factor(1), fill = LorF)) +
   geom_bar(color = "black") +
