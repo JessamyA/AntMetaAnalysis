24032042c0635728b59934ce2ba6b8757959ebd1
JessamyA <jsa529@york.ac.uk>
2021-04-28 14:49:38 +0100

    Post-hoc analysis on all fisher's exact tests

--- Analysis/HypothesisAnalysis.R
+++ Analysis/HypothesisAnalysis.R
@@ -8,6 +8,7 @@ library(ggplot2)
 library(ape)
 library(gplots)
 library(ggpubr)
+library(rcompanion)
 
 #Import data 
 Rank <- read.table("Data/Ranking.txt", header = TRUE, sep = "\t")
@@ -231,6 +232,29 @@ ggplot(AStoType, aes(x = Subfamily, y = X., fill = Type)) +
 StoTypeAnalysis <- read.delim("Data/StorageTypes/StoTypeTable.txt", header = TRUE, row.names = 1, sep = "\t")
 TABStoTypeAnalysis <- as.table(as.matrix(StoTypeAnalysis))
 fisher.test(StoTypeAnalysis)
+pairwiseNominalIndependence(TABStoTypeAnalysis,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+
+#                Comparison p.Fisher p.adj.Fisher
+#1  Formicinae : Myrmicinae 2.76e-08     2.76e-08
+#2  Formicinae : Ponerinae  1.08e-37     3.24e-37
+#3  Myrmicinae : Ponerinae  2.49e-16     3.74e-16
+
+TStoTypeAnalysis <- t(StoTypeAnalysis)
+TTABStoTypeAnalysis <- as.table(as.matrix(TStoTypeAnalysis))
+pairwiseNominalIndependence(TTABStoTypeAnalysis,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+
+#  Comparison p.Fisher p.adj.Fisher
+#1      E : I 8.67e-36     2.60e-35
+#2      E : L 3.94e-06     5.91e-06
+#3      I : L 8.94e-02     8.94e-02
 
 #MOBILITY
 #Normal
@@ -299,6 +323,51 @@ ggplot(AMobType, aes(x = Subfamily, y = X., fill = Type)) +
 MobTypeAnalysis <- read.delim("Data/MobilityTypes/MobTypeTable.txt", header = TRUE, row.names = 1, sep = "\t")
 TABMobTypeAnalysis <- as.table(as.matrix(MobTypeAnalysis))
 fisher.test(MobTypeAnalysis, simulate.p.value = TRUE)
+pairwiseNominalIndependence(TABMobTypeAnalysis,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+
+#                       Comparison p.Fisher p.adj.Fisher
+#1  Amblyoponinae : Dolichoderinae 9.48e-30     2.21e-29
+#2       Amblyoponinae : Dorylinae 4.45e-19     9.35e-19
+#3    Amblyoponinae : Ectatomminae 2.03e-35     7.11e-35
+#4      Amblyoponinae : Formicinae 2.81e-31     8.43e-31
+#5      Amblyoponinae : Myrmicinae 6.93e-30     1.82e-29
+#6       Amblyoponinae : Ponerinae 1.33e-13     2.54e-13
+#7      Dolichoderinae : Dorylinae 2.21e-59     1.55e-58
+#8   Dolichoderinae : Ectatomminae 2.14e-02     2.25e-02
+#9     Dolichoderinae : Formicinae 3.74e-06     5.24e-06
+#10    Dolichoderinae : Myrmicinae 9.96e-02     9.96e-02
+#11     Dolichoderinae : Ponerinae 9.53e-06     1.25e-05
+#12       Dorylinae : Ectatomminae 2.21e-59     1.55e-58
+#13         Dorylinae : Formicinae 2.13e-51     1.12e-50
+#14         Dorylinae : Myrmicinae 5.55e-60     1.17e-58
+#15          Dorylinae : Ponerinae 6.91e-42     2.90e-41
+#16      Ectatomminae : Formicinae 2.52e-04     3.11e-04
+#17      Ectatomminae : Myrmicinae 2.58e-03     2.85e-03
+#18       Ectatomminae : Ponerinae 2.35e-09     4.11e-09
+#19        Formicinae : Myrmicinae 1.03e-03     1.20e-03
+#20         Formicinae : Ponerinae 6.14e-09     9.92e-09
+#21         Myrmicinae : Ponerinae 2.20e-06     3.30e-06
+
+TMobTypeAnalysis <- t(MobTypeAnalysis)
+TTABMobTypeAnalysis <- as.table(as.matrix(TMobTypeAnalysis))
+pairwiseNominalIndependence(TTABMobTypeAnalysis,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3,
+                            simulate.p.value = TRUE)
+
+#  Comparison p.Fisher p.adj.Fisher
+#1      A : I    5e-04        6e-04
+#2      A : N    5e-04        6e-04
+#3      A : U    5e-04        6e-04
+#4      I : N    5e-04        6e-04
+#5      I : U    1e-03        1e-03
+#6      N : U    5e-04        6e-04
 
 ############################################################################################## Phylogenetic Tree
 
@@ -358,17 +427,60 @@ balloonplot(t(TABFisherAdjHM), main = "H+M",
 
 ggballoonplot(TABFisherAdjHM)
 
-#FISHERS TEST
+#FISHERS TESTS
+#NO
 fisher.test(FisherNOAdj)
 #p-value = 0.3571
+pairwiseNominalIndependence(TABFisherNOAdj,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+#StoY:StoN    p = 0.357
+TNOAdj <- t(FisherNOAdj)
+TTABNOAdj <- as.table(as.matrix(TNOAdj))
+pairwiseNominalIndependence(TTABNOAdj,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+#MobY:MobN    p = 0.357
+
+#+H
 fisher.test(FisherAdjH)
 #p-value = 0.2305
+pairwiseNominalIndependence(TABFisherAdjH,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+#StoY:StoN    p = 0.23
+TAdjH <- t(FisherAdjH)
+TTABAdjH <- as.table(as.matrix(TAdjH))
+pairwiseNominalIndependence(TTABAdjH,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+#MobY:MobN    p = 0.23
+
+#+H+M
 fisher.test(FisherAdjHM)
 #p-value = 0.006366
-
-
-
-
+pairwiseNominalIndependence(TABFisherAdjHM,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+#StoY:StoN    p = 0.00637
+TAdjHM <- t(FisherAdjHM)
+TTABAdjHM <- as.table(as.matrix(TAdjHM))
+pairwiseNominalIndependence(TTABAdjHM,
+                            fisher = TRUE,
+                            gtest  = FALSE,
+                            chisq  = FALSE,
+                            digits = 3)
+#MobY:MobN    p = 0.00637
 
 
 
